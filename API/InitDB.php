c21fbcd019ee7374178fa4fb034663534f33bc79
Iryna Martynchuk <lilu108@yandex.ru>
2018-02-14 12:58:12 +0100

    Merge origin/master

---
+++ API/InitDB.php
@@ -0,0 +1,25 @@
+<?php
+header('Content-Type: application/json');
+$OK = true;
+$db = new mysqli("localhost", "root","root","Kunde");
+if($db->connect_error)
+{
+   $OK=false;
+}
+$fil= file_get_contents('InitDB.sql');
+
+$res = $db->multi_query($fil);
+$db->close();
+if(!$res)
+{
+    $OK=false;
+}
+if($OK)
+{
+    echo json_encode("OK");
+}
+else
+{
+    echo json_encode("Feil");
+} 
+
